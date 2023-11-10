b6d407ea460c8ccc02611d7e3a94c10d34e57c53
Ahmad-Su01 <ahmadsulaibi01@gmail.com>
2023-11-10 21:00:02 +0200

    first commit

---
+++ start.py
@@ -0,0 +1,92 @@
+
+
+""" 
+# Syntax / Semantics
+# bad semantics/ Good syntax
+a = 10
+b = "Georgiana"
+
+sum = a / b # 10 + 10 = 20
+
+print(sum) # 20
+"""
+
+# Syntax: Variable = Value
+name = "georgiana"
+height = 180
+class_1 = "mathematica"
+
+# DataType:
+# integer: hole number e.g. 12, 20. float: 12.3, -52.678865, 
+# character/char: 'a', 'b', ..., 'z'. String: "AHMAD", "georg", "ae"
+# Quotetion: ', "
+#e.g. area of cerc = pi * radius^2
+pi = 3.14 # float
+radius = 12
+
+# Single comment
+""" 
+Multi comment
+sadsa
+sadsadsa
+fsda
+fdsa
+fads
+fdsa
+"""
+
+# Facem sumajul din 2 numere
+# sum = a + b
+
+# adaugi
+# Syntax: import nameOfFile
+
+# Operatiuni de matematica
+# plus: +, minus: -, multiplication: *, impartire: /, putere: **, floor: //, Modulus/Restul: %
+
+# Modulus
+# num_1 = 5
+# num_2 = 3
+
+# print(num_1 // num_2)
+# print(num_1 % num_2) # restul
+# print(num_1 ** num_2)
+
+# Syntax: input()
+# Userul poate sa adouge value/numarul/numele
+# str: transformeze catre string ""
+# numar = 2, str(numar) = "2"
+# numar = "2", int(numar) = 2
+# nume = str(input("Adauga numele tau: "))
+# numar = int(input("numar = ")) # numere intregi
+# num_float = float(input("float number = ")) # numere intregi si cu decimal(.)
+
+# print(nume)
+# print(numar)
+# print(num_float)
+
+# Output: afisezi
+# Syntax: print()
+# print(" ", variable)
+# print(f"dsafdsa {variable} sadsa {vairable}")
+
+# carti = 100
+# print(f"Maria a cumparat {carti} matematica {carti}")
+
+
+# Syntax: slicing[start:end:step]
+
+# val = [1, 2, 3]
+# rev_val = val[::-1]
+
+# print(f"{val} => {rev_val}")
+
+# a = 123, a = [start: end: step] // slicing
+# a = [1, 2, 3] => [:] => copy
+# [1:] => [2, 3]
+# [:2] => [1, 2]
+# [:-1] => [3]
+# [::-1] => reverse
+
+
+# If..else, loops/enumerate, functions.
